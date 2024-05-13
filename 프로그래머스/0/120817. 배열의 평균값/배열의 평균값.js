1cfb70077f7e2e0421becafa52e07876c6ceac3b
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-05-14 08:46:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let sum = 0;
+    
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];    
+    }
+    
+    let average = sum / numbers.length;
+    
+    return average;
+}
