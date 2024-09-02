10ded818944dd45b938855dab0e2938372775dad
봄 <167315197+bom0320@users.noreply.github.com>
2024-09-02 10:37:03 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,14 @@
+function solution(a, b, c) {
+    // 세 숫자가 모두 같은 경우
+    if (a === b && b === c) {
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
+    }
+    // 두 숫자가 같은 경우
+    else if (a === b || b === c || a === c) {
+        return (a + b + c) * (a**2 + b**2 + c**2);
+    }
+    // 세 숫자가 모두 다른 경우
+    else {
+        return a + b + c;
+    }
+}
